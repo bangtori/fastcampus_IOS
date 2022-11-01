f13a91633f298f87f4610437e08753d23eee1189
Tori <ttang1135@naver.com>
2022-11-01 15:02:11 +0900

    아울렛 및 액션 함수 연결

--- QuotesGenerator/QuotesGenerator/ViewController.swift
+++ QuotesGenerator/QuotesGenerator/ViewController.swift
@@ -9,11 +9,16 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet var lblQuotes: UILabel!
+    @IBOutlet var lblName: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func btnChangeQuote(_ sender: UIButton) {
+    }
+    
 }
 
