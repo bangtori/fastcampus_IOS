809df9374afd46bb66973b9ec3466fb8530a172f
Tori <ttang1135@naver.com>
2022-11-27 14:07:29 +0900

    TextView 보더 설정

--- Diary/Diary/WriteDiaryViewController.swift
+++ Diary/Diary/WriteDiaryViewController.swift
@@ -16,10 +16,14 @@ class WriteDiaryViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        self.configureContentsTextView()
     }
-    
+    private func configureContentsTextView(){
+        let borderColor = UIColor(red: 220/255, green: 220/255, blue: 220/255, alpha: 1.0)
+        self.contentsTextView.layer.borderColor = borderColor.cgColor
+        self.contentsTextView.layer.borderWidth = 0.5
+        self.contentsTextView.layer.cornerRadius = 5.0
+    }
     @IBAction func tapConfrimButton(_ sender: UIBarButtonItem) {
     }
     
