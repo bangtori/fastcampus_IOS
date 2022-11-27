92e90521344d64720e1ca3eae4943a737eb4dcfa
Tori <ttang1135@naver.com>
2022-11-27 14:02:02 +0900

    아울렛 및 액션함수 연결

--- Diary/Diary/DiaryCell.swift
+++ Diary/Diary/DiaryCell.swift
@@ -2,11 +2,13 @@
 //  DiaryCell.swift
 //  Diary
 //
-//  Created by 방유빈 on 2022/11/24.
+//  Created by 방유빈 on 2022/11/27.
 //
 
 import UIKit
 
 class DiaryCell: UICollectionViewCell {
+    @IBOutlet weak var titleLabel: UILabel!
+    @IBOutlet weak var dateLabel: UILabel!
     
 }
--- Diary/Diary/WriteDiaryViewController.swift
+++ Diary/Diary/WriteDiaryViewController.swift
@@ -9,21 +9,18 @@ import UIKit
 
 class WriteDiaryViewController: UIViewController {
 
+    @IBOutlet weak var titleTextField: UITextField!
+    @IBOutlet weak var contentsTextView: UITextView!
+    @IBOutlet weak var dateTextField: UITextField!
+    @IBOutlet weak var confirmButton: UIBarButtonItem!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    @IBAction func tapConfrimButton(_ sender: UIBarButtonItem) {
     }
-    */
-
+    
 }
